8cadd72b1818f81812b047289a407618ba94d826
Leon Xu <dcwinfield362@gmail.com>
2022-09-03 11:56:57 -0700

    doc: add ToogleColorMode from utils directory

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import ToggleColorMode from "./utils/ToggleColorMode";
 import App from './components/App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <ToggleColorMode>
     <BrowserRouter>
         <React.StrictMode>
             <App />
         </React.StrictMode>
     </BrowserRouter>
+    </ToggleColorMode>
 );
