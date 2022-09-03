2d6884389f6baa4b61d30ee116b211e0832b34b7
Leon Xu <6479451-leonxu260@users.noreply.gitlab.com>
2022-09-03 20:22:11 +0000

    file: create index.js file in the components directory that contains all the...

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import { useRef } from 'react';
+import { CssBaseline} from "@mui/material";
+import { Routes, Route } from 'react-router-dom';
+
+import useStyles from './styles';
+import useAlan from './Alan';
+
+import { Actors, Movies, MovieInfo, Navbar, Profile } from './index'
+
+function App() {
+    const classes = useStyles();
+    const alanBtnContainer = useRef();
+
+    useAlan();
+
+  return (
+   <div className={classes.root}>
+       <CssBaseline />
+       <Navbar />
+       <main className={classes.content}>
+           <div className={classes.toolbar}>
+               <Routes>
+                   <Route exact path={'/'} element={<Movies />}/>
+                   <Route exact path={'/approved'} element={<Movies />}/>
+                   <Route exact path={'/movie/:id'} element={<MovieInfo />}/>
+                   <Route exact path={'/actors/:id'} element={<Actors />}/>
+                   <Route exact path={'/profile/:id'} element={<Profile />}/>
+               </Routes>
+           </div>
+       </main>
+       <div ref={alanBtnContainer}/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,13 @@
+import { useState} from "react";
+import { TextField, InputAdornment } from '@mui/material';
+import { Search as SearchIcon } from '@mui/icons-material';
+
+import useStyles from './styles';
+
+function Search() {
+    return (
+        <></>
+    )
+}
+
+export default Search;
