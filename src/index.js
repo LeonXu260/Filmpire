5f737dc8246184972ba31f1ed104e3c735fc7079
Leon Xu <dcwinfield362@gmail.com>
2022-09-03 12:16:27 -0700

    doc: import Provider from react-redux and store from store directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import ToggleColorMode from "./utils/ToggleColorMode";
 import App from './components/App';
 import './index.css';
