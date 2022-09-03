1837dd18af4e8b1011a3dd61ae517e2018460977
Leon Xu <dcwinfield362@gmail.com>
2022-09-03 12:08:02 -0700

    doc: add App function with styles and components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,22 @@
+import { useRef } from 'react';
+import { CssBaseline} from "@mui/material";
+import { Routes, Route } from 'react-router-dom';
+
+import useStyles from './styles'
+
+import { Navbar } from './index'
+
 function App() {
+    const classes = useStyles();
   return (
-   <></>
+   <div className={classes.root}>
+       <CssBaseline />
+       <Navbar />
+       <main className={classes.content}>
+           <div className={classes.toolbar}>
+           </div>
+       </main>
+   </div>
   );
 }
 
