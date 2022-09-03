476d78b3b8e44b444b02da513bae5080991d56e3
Leon Xu <dcwinfield362@gmail.com>
2022-09-03 12:07:08 -0700

    file: create Sidebar directory with Sidebar component and its stylesheet

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { Divider, List, ListItem, ListItemText ,ListSubheader, ListItemIcon, Box, CircularProgress} from "@mui/material";
+import { Link } from 'react-router-dom';
+import { useTheme} from "@mui/styles";
+import { useDispatch, useSelector} from "react-redux";
+
+import useStyles from './styles';
+
+function Sidebar() {
+    return (
+        <></>
+    )
+}
+
+export default Sidebar;
